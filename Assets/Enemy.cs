d051792032e6c24ae6b8a2da5f82244b7f6364e6
Screwdriver1982 <60478923+Screwdriver1982@users.noreply.github.com>
2020-02-04 21:16:47 +0300

    Enemy and Player

---
+++ Assets/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Player playerVar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerVar.playerName = playerVar.playerName + "The Brave";
+        Debug.Log("Hello " + playerVar.playerName + "! I'm your enemy!");
+    }
+
+    public void RunAway()
+    {
+        Debug.Log("I'm Scared!");
+    }
+
+    void Win()
+    { 
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Come on " + playerVar.playerName + "! Let's fight!"); 
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string playerName;
+    public Enemy enemyVar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello! My name is" + playerName); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Oh no!");
+    }
+}
