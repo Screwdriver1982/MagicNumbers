4a6cac20aa450fca6d9e6e38fcb7d83b73518efd
Screwdriver1982 <60478923+Screwdriver1982@users.noreply.github.com>
2020-02-13 21:20:41 +0300

    button up and down MSH PRO

---
+++ Assets/Script/MagicNumbersUI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class MagicNumbersUI : MonoBehaviour
+{
+    public int minNumber = 1;
+    public int maxNumber = 1000;
+    public int guess;
+    int stepNumber = 0;
+    public Text guessTxt;
+    public TextMeshProUGUI stepNumberTxt;
+
+    void UpdateGuess()
+    {
+        guess = Random.Range(minNumber, maxNumber+1);
+        stepNumber = stepNumber + 1;
+        stepNumberTxt.text = stepNumber.ToString();
+        guessTxt.text = "" + guess;
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateGuess();
+    }
+
+    // Update is called once per frame
+    public void Click(int upOrDown)
+    {
+        if (upOrDown == 1)
+        {
+            if (guess == maxNumber-1)
+            {
+                guess = maxNumber;
+
+            }
+            else
+            {
+                minNumber = guess+1;
+                UpdateGuess();
+            }
+        }
+
+        else if (upOrDown == 0)
+        {
+            maxNumber = guess-1;
+            UpdateGuess();
+        }
+
+    }
+}
---
+++ Assets/Script/ScenesLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesLoader : MonoBehaviour
+{
+
+    public void LoadNextLevel()
+    {
+        int activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(activeSceneIndex + 1 );
+
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
